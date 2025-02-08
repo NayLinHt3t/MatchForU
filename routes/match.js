eadfb90b320e1dbc6dc298a48b9987a1a5b91c8a
naeyar <naeyar347@gmail.com>
2025-02-08 11:20:07 +0700

    done with match

--- routes/match.js
+++ routes/match.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
 const matchController = require('../controllers/match');
+const recommendController = require('../controllers/recommend');
+router
+  .route('/recommend')
+  .get(authController.protected, recommendController.getRandomUsers);
 router.route('/match').get(authController.protected, matchController.getMatch);
 router.route('/like').post(authController.protected, matchController.likeUser);
 router
